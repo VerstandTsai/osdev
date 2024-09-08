5f7864679ed1ce2520984166fb17bd7115350d89
VerstandTsai <hackertsaigenz@gmail.com>
2024-09-09 01:24:34 +0800

    Created struct Teletype and RingBuffer

--- include/tty.h
+++ include/tty.h
@@ -1,8 +1,20 @@
 #ifndef _TTY_H
 #define _TTY_H
 
+struct RingBuffer {
+    char buffer[256];
+    unsigned char head;
+    unsigned char tail;
+};
+
+void ring_init(struct RingBuffer *ring);
+void ring_push(struct RingBuffer *ring, char value);
+char ring_pop(struct RingBuffer *ring);
+int ring_empty(struct RingBuffer *ring);
+
 void console_init();
 void console_write(const char *src, int count);
+
 void tty_init();
 void tty_write(int channel, const char *src, int count);
 
--- src/tty.c
+++ src/tty.c
@@ -1,4 +1,18 @@
 #include <tty.h>
+#include <termios.h>
+
+void ring_init(struct RingBuffer *ring)             { ring->head = ring->tail = 0; }
+void ring_push(struct RingBuffer *ring, char value) { ring->buffer[ring->tail++] = value; }
+char ring_pop(struct RingBuffer *ring)              { return ring->buffer[ring->head++]; }
+int ring_empty(struct RingBuffer *ring)             { return ring->head == ring->tail; }
+
+struct Teletype {
+    struct termios termios;
+    struct RingBuffer input_queue;
+    struct RingBuffer output_queue;
+    void (*write)(struct RingBuffer*);
+    int allocated;
+};
 
 void tty_init() {}
 
